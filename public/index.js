674bcef475500f6361717d2f452e1fba144230a6
joaoartur-mk0 <joaoartur655@gmail.com>
2025-11-25 11:00:23 -0300

    tt

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path'); 
+const app = express();
+const port = 3200;
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
